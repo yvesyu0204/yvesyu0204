d9953ea77a5b4a6bec805423d79d4cbdae6300f8
yvesyu0204 <141757898+yvesyu0204@users.noreply.github.com>
2023-08-09 02:14:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from line_bot_api import *
-from events.basic import  *
-from events.oil import *
+from flask import Flask,request,abort
+from linebot import(LineBotApi,WebhookHandler,exceptions)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import  *
 
 app=Flask(__name__)
 
