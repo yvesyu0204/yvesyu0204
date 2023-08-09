68b48de8a75e35854cdf9ac7429cba1b4300f3f6
yvesyu0204 <141757898+yvesyu0204@users.noreply.github.com>
2023-08-09 11:15:52 +0800

    Add files via upload

--- events/Msg_Template.py
+++ events/Msg_Template.py
@@ -3,24 +3,23 @@ from line_bot_api import *
 
 
 def stock_reply_other(stockNumber):
-    content_text = '即時股價和K線圖'
+    content_text = "即時股價和K線圖"
     text_message = TextSendMessage(
                                 text = content_text ,
                                 quick_reply=QuickReply(
-                                    items=[
+                                   items=[
+                                       QuickReplyButton(
+                                                action=MessageAction(
+                                                    label="#+股票代號查詢", 
+                                                    text="#"+stockNumber,
+                                                )
+                                       ),
                                         QuickReplyButton(
                                                 action=MessageAction(
-                                                    label='#+股票代號查詢',
-                                                    text='#' + stockNumber,
+                                                    label="K線圖", 
+                                                    text="@K"+stockNumber
                                                 )
-                                        ),
-                                        QuickReplyButton(
-                                                action=MessageAction(
-                                                    label='K線圖',
-                                                    text='@K' + stockNumber
-                                                )
-                                        ),
-                                    ]
-                                ))
-    
+                                       ),
+                                       ]
+                            ))
     return text_message
